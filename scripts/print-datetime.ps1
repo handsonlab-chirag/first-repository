f4d7efe436ddaf90a78d33613a4a37846fc246b8
handsonlab-chirag <handsonlab.chirag@gmail.com>
2025-01-09 06:56:22 +0000

    Change to use powershell script

---
+++ scripts/print-datetime.ps1
@@ -0,0 +1,3 @@
+# scripts/print-datetime.ps1
+$currentDateTime = Get-Date
+Write-Output "The current date and time is: $currentDateTime"
--- scripts/testscript.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-ARG=$1
-
-echo $ARG
-echo "Current date and time: $(date)"
